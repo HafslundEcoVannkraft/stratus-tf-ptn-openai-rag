8a7ed5d366cfbb4507589eeaa0bbc3510a7fcc99
Magnus Longva <magnus@longva.email>
2025-03-09 16:00:42 +0100

    search service ref main

--- search_service.tf
+++ search_service.tf
@@ -1,5 +1,6 @@
 module "search_service" {
-  source                  = "github.com/HafslundEcoVannkraft/stratus-tf-res-search-searchservice?ref=v1.1.1"
+  #source                  = "github.com/HafslundEcoVannkraft/stratus-tf-res-search-searchservice?ref=v1.1.1"
+  source                  = "github.com/HafslundEcoVannkraft/stratus-tf-res-search-searchservice?ref=main"
   rg_id                   = data.azapi_resource.resource_group.id
   search_service_name     = var.search_service_name
   ip_rules                = []
